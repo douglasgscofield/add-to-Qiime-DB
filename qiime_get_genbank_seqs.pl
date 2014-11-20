a9bd7b4b05c80309b92f7b8971a0c3caa27a12ea
Douglas G. Scofield <douglasgscofield@gmail.com>
2014-11-20 10:40:17 +0100

    Commit of modified scripts

--- qiime_get_genbank_seqs.pl
+++ qiime_get_genbank_seqs.pl
@@ -1,46 +1,112 @@
-#!/opt/local/bin/perl5.18 
+#!/usr/bin/env perl 
 
-eval 'exec /opt/local/bin/perl5.18  -S $0 ${1+"$@"}'
+    eval 'exec /usr/bin/env perl  -S $0 ${1+"$@"}'
     if 0; # not running under some shell
 
 
 =head1 NAME
 
-bp_download_query_genbank - script to query Genbank and retrieve records
+qiime_get_genbank_seqs - Retrieve Genbank sequences and produce output useful
+                         for augmenting a Qiime reference database.
+ 
+This script is derived from the BioPerl script bp_download_query_genbank.pl.
+Differences include restricting to FASTA format and restricting to the
+nucleotide database, and the additional options --accession/--noaccession,
+--taxonomy/--notaxonomy, and --primary for controlling output and naming of
+output sequences, removal of the query file option, and removal of the output 
+file option.
+
+Two files are output: a file of FASTA-format sequences to STDOUT, and a file of
+IDs to STDERR.
+
+The IDs provided for the query can include additional taxonomy information tacked
+onto the end (see the --taxonomy option) and can include additional information
+after the IDs separated by whitespace.  The ID for query is taken from just the
+first entry (possibly modified according to --taxonomy).  The IDs output to stderr
+are the full ID provided, including any information following whitespace, in the
+same order as the sequences in the sequence file written to STDOUT.  Note that
+because of the nature of Genbank queries, the output is not likely to be in the
+same order as the query IDs in the gifile, but the files written to STDOUT and
+STDERR *will be* in the same order.
 
 =head1 USAGE
 
- bp_download_query_genbank --query "Neurospora[ORGN]" --db nucest -o Ncrassa_ESTs.fa --format fasta
+qiime_get_genbank_seqs --query 18481572 > seqs.fa 2> seqs_ids.txt
 
- bp_download_query_genbank --queryfile 'filewithquery' --db nucest -o Ncrassa_ESTs.fa --format fasta 
+qiime_get_genbank_seqs --gifile file_with_gis.txt > seqs.fa 2>seqs_ids.txt
 
 =head2 Other options
 
- Provide ONE of:
+Provide ONE of:
 
-  -q --query query string OR
-  --queryfile profile file with query OR
-  --gi --gis --gifile file with list of GIs to download
+=over
 
- Database type:
+=item -q | --query string
 
- -d --db database (nucleotide [default], nucest, protein, )
+string is an ID to use to query GenBank, or
 
- -o --out --outfile output file (results are displayed on screen otherwise)
- -f --format sequence file output format (fasta by default)
- -v --verbose debugging output
+=back
 
+=over
+
+=item --gi --gis --gifile file 
+
+where file contains a list of GIs to download.
+
+=back
+
+This script is modified to support its use with augmenting a Qiime reference database.
+The --taxonomy and --accession options are On by default for this reason.
+
+=over
+
+=item -t --taxonomy
+
+query is in the form of gi_taxonid, as also expected by taxonomy.pl the sequence display ID will also be gi_taxonid.  Only the first whitespace-separated field will be used
+
+=item --notaxonomy
+
+used to turn off --taxonomy option, which is On by default
+
+=item -a --accession
+
+write table of ID <TAB> accession number to STDERR
+
+=item --noaccession
+
+used to turn off --accession option, which is On by default
+
+=item -p --primary
+
+prefix the primary ID to the display_id before writing the sequence CANNOT co-occur with --taxonomy if it co-occurs with --verbose, STDERR will be all mixed up
+
+=item -v --verbose
+
+debugging output
+
+=back
+ 
 =head2 Query options
 
- --maxids maximum number of IDs to retrieve in a set (100 at a time by default)
- --reldate 
- --maxdate maxdate for a record
- --mindate minimum date for record
- --datetype edat or mdat (entered or modified)
+=over
+
+=item --maxids maximum number of IDs to retrieve in a set (default 100)
+
+=item --reldate 
+
+=item --maxdate maxdate for a record
+
+=item --mindate minimum date for record
+
+=item --datetype edat or mdat (entered or modified)
+
+=back
 
 =head1 AUTHOR Jason Stajich
 
 Jason Stajich, jason-AT-bioperl.org
+
+Modified by Douglas Scofield to do Qiime-oriented work, douglas.scofield@ebc.uu.se
 
 =cut
 
@@ -51,25 +117,29 @@ use Bio::DB::GenPept;
 use Bio::DB::Query::GenBank;
 use Bio::SeqIO;
 use Getopt::Long;
+use Pod::Usage;
 
-my ($queryfile,$outfile,$format,$debug,%options);
-
+my ($format,$debug,$primary,$accession,$taxonomy,%options);
+# for Qiime work, all four of these should remain unchanged
+$accession = 1;
+$taxonomy = 1;
 $format = 'fasta';
+$options{'-db'} = 'nucleotide';
 
 $options{'-maxids'} = '100';
-$options{'-db'} = 'nucleotide'; # can be nucleotide, nucest, protein 
 my $gifile;
 GetOptions(
-		   'h|help' => sub { exec('perldoc', $0); 
+		      'h|help' => sub { exec('perldoc', $0); 
 									exit(0);
 								},
 			  'v|verbose'       => \$debug,
-			  'f|format:s'      => \$format,
-			  'queryfile:s'     => \$queryfile,
-			  'o|out|outfile:s' => \$outfile,
 			  'gi|gifile|gis:s' => \$gifile,
+			  'p|primary'       => \$primary,
+			  'a|accession'     => \$accession,
+              'noaccession'     => sub { $accession = 0 },
+              't|taxonomy'      => \$taxonomy,
+              'notaxonomy'      => sub { $taxonomy = 0 },
 			  # DB::Query options	   
-			  'd|db:s'     => \$options{'-db'},
 			  'mindate:s'  => \$options{'-mindate'},
 			  'maxdate:s'  => \$options{'-maxdate'},
 			  'reldate:s'  => \$options{'-reldate'}, 
@@ -77,55 +147,76 @@ GetOptions(
 			  'maxids:i'   => \$options{'-maxids'},
 			  'q|query:s'  => \$options{'-query'},
 			 );
+die "--taxonomy and --primary cannot co-occur" if $taxonomy and $primary;
 
-my $out;
+my $out = Bio::SeqIO->new(-format => $format); # write to STDOUT
 
-if( $outfile ) {
-	$out = Bio::SeqIO->new(-format => $format,
-								  -file   => ">$outfile");
-} else {
-	$out = Bio::SeqIO->new(-format => $format); # write to STDOUT
+my $dbh = Bio::DB::GenBank->new(-verbose => $debug);
+
+sub dump_seq($) {
+    my $seq = shift;
+    print STDERR "seq->primary_id       = ".$seq->primary_id."\n";
+    print STDERR "seq->display_id       = ".$seq->display_id."\n";
+    print STDERR "seq->accession_number = ".$seq->accession_number."\n";
+    print STDERR "seq->desc             = ".$seq->desc."\n";
 }
-
-my $dbh;
-if( $options{'-db'} eq 'protein' ) {
-	$dbh = Bio::DB::GenPept->new(-verbose => $debug);
-} else {
-	$dbh = Bio::DB::GenBank->new(-verbose => $debug);
+my %taxonomy_ids;
+sub process_id($) {
+    $_ = shift;
+    print STDERR "\$_ = $_\n" if $debug;
+    my ($id, undef) = split;
+    print STDERR "\$id = $id\n" if $debug;
+    my @id = split /_/, $id;
+    die "invalid id for taxonomy: $id" if scalar @id != 2;
+    $taxonomy_ids{$id[0]} = $id;
+    return $id[0];
+}
+sub process_seq($) {
+    my $seq = shift;
+    if ($debug) { print STDERR "process_seq BEFORE\n"; dump_seq($seq); }
+    if ($taxonomy) {
+        $seq->desc($seq->accession_number." ".$seq->display_id." ".$seq->desc);
+        $seq->display_id($taxonomy_ids{$seq->primary_id});
+    } elsif ($primary) {
+        $seq->desc($seq->accession_number." ".$seq->display_id." ".$seq->desc);
+        $seq->display_id($seq->primary_id."_".$seq->display_id);
+    }
+    print STDERR $seq->display_id."\t".$seq->accession_number."\n" if $accession;
+    if ($debug) { print STDERR "process_seq AFTER\n"; dump_seq($seq); }
 }
 my $query;
 if( $gifile ) {
 	my @ids;
 	open( my $fh => $gifile ) || die $!;
 	while(<$fh>) {
-		push @ids, split;
+        if ($taxonomy) {
+		    push @ids, $_;
+        } else {
+		    push @ids, split;
+        }
 	}
 	close($fh);	
 	while( @ids ) {
 		my @mini_ids = splice(@ids, 0, $options{'-maxids'});
+        @mini_ids = map { process_id($_) } @mini_ids if $taxonomy;
 		$query = Bio::DB::Query::GenBank->new(%options,
 														  -ids => \@mini_ids,
 														 );
 		my $stream = $dbh->get_Stream_by_query($query);
 		while( my $seq = $stream->next_seq ) {
+            process_seq($seq);
 			$out->write_seq($seq);
 		}
 	}
 	exit;
 } elsif( $options{'-query'}) {
+    $options{'-query'} = process_id($options{'-query'}) if $taxonomy;
 	$query = Bio::DB::Query::GenBank->new(%options);
-} elsif( $queryfile ) {
-	open(my $fh => $queryfile) || die $!;
-	while(<$queryfile>) {
-		chomp;
-		$options{'-query'} .= $_;
-	}
-	$query = Bio::DB::Query::GenBank->new(%options);
-	close($fh);
 } else {
 	die("no query string or gifile\n");
 }
 my $stream = $dbh->get_Stream_by_query($query);
 while( my $seq = $stream->next_seq ) {
+    process_seq($seq);
 	$out->write_seq($seq);
 }
--- qiime_get_taxonomy_from_seqs.pl
+++ qiime_get_taxonomy_from_seqs.pl
@@ -1,4 +1,9 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
+
+# Create Qiime-compatible taxonomy from sequence input file
+# 
+# Modified from:
+#
 # Script: taxonomy.pl
 # Description: Takes an ID file and determines the taxonomy hierarchy for each species 
 # Author: Steven Ahrendt
@@ -21,26 +26,59 @@
 # where
 #  someID = either Accession or GI
 # and
-#  someTaxID = either NCBI Taxonomy ID or "Genus species" identifier
+#  someTaxID = NCBI Taxonomy ID
 #####################################
 
 use strict;
+use warnings;
 use Bio::DB::Taxonomy;
 use File::Spec;
 use Bio::SeqIO;
 use Getopt::Long;
 
 my ($name);
-my $numseqs = 0;
-my $unidentified = 0; # Unidentified organisms
+my $n_seqs = 0;
+my $n_unidentified = 0; # Unidentified organisms
+my $n_excluded = 0; # Excluded organisms
+my $regexp_excluded; # Regexp to exclude
 my $show = 0; # Show or hide ranks with value "no rank"
+my $o_qiime = 1; # produce taxon descriptions compatible with qiime
+my $o_first = 0; # if there are multiple taxon ids separated by ' ' or ';', take the first rather than failing
+my $o_retry = 1; # if there are multiple taxon ids separated by ' ' or ';', try them in turn rather than failing
+my $o_taxon = 1;
+my $idformat = 'HUDS%0.4u.01FU_%s';  # field 1 is filled with $id1, field 2 is filled with accession
+my $id1 = 1;
+my $accessionfile;
 my $outext = "";
 # Handle command-line options
 my $debug;
 
-GetOptions( 's|show!'=> sub { $show = 1; $outext = '.norank'},
-	    'verbose|debug!' => \$debug,
+GetOptions( 's|show!' => sub { $show = 1; $outext = '.norank'},
+            'q|qiime' => \$o_qiime,
+            'noqiime' => sub { $o_qiime = 0 },
+            'accessionfile=s' => \$accessionfile,
+            'f|first' => \$o_first,
+            'idformat' => \$idformat,
+            'id1' => \$id1,
+            't|taxon' => \$o_taxon,
+            'notaxon' => sub { $o_taxon = 0 },
+            'retry' => \$o_retry,
+            'noretry' => sub { $o_retry = 0 },
+            'exclude=s' => \$regexp_excluded,
+	        'verbose|debug!' => \$debug,
 	    );
+die "must use --accessionfile to supply a file of IDs and accession numbers" if $o_qiime and not $accessionfile;
+die "must use --idformat to supply an ID format to use" if $o_qiime and not $idformat;
+die "only one of --first and --retry may be specified" if $o_first and $o_retry;
+my %ACCESSION;
+if ($o_qiime) {
+    open(my $fh, "<", $accessionfile) or die "could not open $accessionfile: $!";
+    while (<$fh>) {
+        chomp;
+        my @l = split;
+        $ACCESSION{$l[0]} = $l[1];
+    }
+}
 
 my $IDfile = shift @ARGV;
 
@@ -58,13 +96,13 @@ sub getName {
 }
 
 ## Make local taxonomy db
-warn( "Constructing flatfile db..\n") if $debug;
 my $nodesfile = "./ncbi/nodes.dmp";
 my $namefile = "./ncbi/names.dmp";
+print STDERR "Loading flatfile taxonomy db from '$nodesfile' and '$namefile'...\n";
 my $taxdb = Bio::DB::Taxonomy->new(-source => 'flatfile',
                                    -nodesfile => $nodesfile,
                                    -namesfile => $namefile);
-warn( "loaded taxonomy db..done\n") if $debug;
+print STDERR "done loading taxonomy db\n";
 
 ## Store filename for output files
 my @f = split(/\./,$IDfile);
@@ -77,66 +115,128 @@ my $seqio = Bio::SeqIO->new(-format => 'fasta',
 			    -file   =>  $IDfile);
 warn "Creating taxonomy hierarchy..\n" if $debug;
 
+my $outfile_seqs = "$filename\.taxonomy.fa";
+my $outfile_taxonomy = "$filename\.taxonomy";
+my $outfile_unidentified = "$filename\.unidentified";
+my $outfile_excluded = "$filename\.excluded";
 my $out = Bio::SeqIO->new(-format=> 'fasta',
-			  -file  => ">$filename\.taxonomy.seqs");
-open(UNID,">$filename\.unidentified");
-open(OUT,">$filename\.taxonomy");
+			              -file  => ">$outfile_seqs");
+open(TAXONOMY,">$outfile_taxonomy");
+open(UNIDENTIFIED,">$outfile_unidentified");
+print STDERR "sequences to    : $outfile_seqs\n";
+print STDERR "taxonomy to     : $outfile_taxonomy\n";
+print STDERR "unidentifieds to: $outfile_unidentified\n";
+if ($regexp_excluded) {
+    open(EXCLUDED,">$outfile_excluded");
+    print STDERR "excluded to     : $outfile_excluded    when taxonomic hierarchy matches '$regexp_excluded'\n"
+}
+# %RANKS keys are acceptable taxonomic ranks found in $curr->rank.  Target of each key is the
+# prefix assigned to that rank for the Qiime taxonomy list
+sub process_name($) {
+    my $n = shift;
+    return sprintf($idformat, $id1++, $ACCESSION{$n});
+}
+my %RANKS = qw/ kingdom k__ phylum p__ class c__ order o__ family f__ genus g__ species s__ /;
 while (my $seq = $seqio->next_seq) { 
-    my $line = $seq->display_name;
-    #print $line,"\n";
+    printf STDERR "processing sequence $n_seqs\n" if (++$n_seqs % 500) == 0;
+    my $display_name = $seq->display_name;
+    #print $display_name,"\n";
     ## GI/Accession number is the first item before the first underscore
     ## Organism name/Taxonomy ID is everything after the first underscore
-    my ($ID,@descriptors) = split(/\_/, $line);
+    my ($ID,@descriptors) = split(/\_/, $display_name);
     my $org = join(" ",@descriptors);
-    
+    my $taxon_descriptors = $org;
+    die "no taxonid found for id '$ID'" if ! @descriptors;
+    die "too many taxonid descriptors for id '$ID': $taxon_descriptors" if not ($o_first or $o_retry) and scalar(@descriptors) > 1;
+    my @o;
+    if ($org =~ /^[\d;]+$/) {
+        if ($org =~ /;/) {
+            if ($o_first or $o_retry) {
+                print STDERR "splitting multi-taxonid descriptor '$org'\n" if $debug;
+                @o = split /;/, $org;
+                $org = shift @o;
+            } else {
+                die "taxonid '$org' must be all-numeric; multiple ids may be separated by ';' if --first or --retry specified";
+            }
+        }
+    }
+
     ## If the input provided is a Taxonomy ID number, 
     ##  get the corresponding organism name
     my $input = $org;
-    if($input =~ /^\d+$/) {
-	$input = ($taxdb->get_taxon(-taxonid => $org))->scientific_name;
+    my $taxonid;
+    if ($input =~ /^\d+$/) {  # we need to look up the taxonid to set $input to its scientific_name
+        my $tax;
+        while ($org) {
+            $tax = $taxdb->get_taxon(-taxonid => $org);
+            if (defined($tax)) {
+                $taxonid = $org;
+                $input = $tax->scientific_name;
+                last;
+            } elsif (! $o_first and @o and $o[0] =~ /^\d+$/) { # more valid taxonids to examine
+                print STDERR "taxonid '$org' is undefined, trying '$o[0]'...\n" if $debug;
+                $org = shift @o;
+            } else { # no valid taxonids
+                print STDERR "undefined taxon for taxonid = '$org'\n" if $debug;
+                print STDERR "no valid taxonid found in '$taxon_descriptors'\n";
+                last;
+            }
+        } 
     }
 
-  ## Using organism name, generate hierarchy
-  my @hierarchy;
-  my %ranks;
-  if(my $curr = $taxdb->get_taxon(-name => $input)) {
-      my $name = getName($curr);
-      #print $name,";";
-      #unshift(@hierarchy,$name);
-      ## Default: hide "no rank"
-      ## Flag: -s = show "no rank"
+    ## Using organism name, generate hierarchy
+    my @hierarchy;
+    if(my $curr = $taxdb->get_taxon(-name => $input)) {
+        my $name = getName($curr);
+        #print $name,";";
+        #unshift(@hierarchy,$name);
 
-      # could also just check and see if $curr->rank is NULL?
-      while($curr) {
-	  
-          if($curr->rank eq "kingdom" || $curr->rank eq "phylum" || $curr->rank eq "class" || $curr->rank eq "order" || $curr->rank eq "family" || $curr->rank eq "genus")
-          {
-	  #print "<",$curr->rank,">;";
-	  $name = getName($curr);
-          #print $name,";";
-	  if($name !~ /^</ || $show)  {
-	      unshift(@hierarchy,$name);
-	  }
-          }
-	  $curr = $curr->ancestor;
-      }
-      #print "\n";
-      print OUT $ID,"\t",join(";",@hierarchy),"\n";
-      $seq->description(join(";",@hierarchy).".");
-      $out->write_seq($seq);      
-  } else {
-      ## Flag missing taxonomic ranks
-      $unidentified = 1;
-      print UNID "$ID $input\n";
-  }
-
+        # could also just check and see if $curr->rank is NULL?
+        while($curr) {
+            if (exists($RANKS{$curr->rank})) {
+                #print "<",$curr->rank,">;";
+                $name = getName($curr);
+                #print $name,";";
+                my $name_no_rank = $name =~ /^</;
+                if (not $name_no_rank and $o_qiime) {
+                    $name = $RANKS{$curr->rank} . $name;  # add [kpcofgs]__ prefix to rank
+                    $name .= " taxonid_".$taxonid if $o_taxon and $taxonid and $curr->rank eq "species";
+                }
+                ## Default: hide "no rank"
+                ## Flag: -s = show "no rank"
+                if (not $name_no_rank or $show)  {
+                    unshift(@hierarchy,$name) unless $curr->rank eq "species" and not $o_qiime;  # original didn't add species
+                }
+            }
+            $curr = $curr->ancestor;
+        }
+        my $hierarchy = join(";", @hierarchy);
+        if ($regexp_excluded and $hierarchy =~ $regexp_excluded) {
+            ++$n_excluded;
+            print STDERR "excluding '$ID $input $hierarchy', matches '$regexp_excluded'\n";# if $debug;
+            print EXCLUDED "$ID\t$taxon_descriptors\t$hierarchy\n";
+            next;
+        }
+        #print "\n";
+        my $final_name = process_name($display_name);
+        print TAXONOMY "$final_name\t$hierarchy\n";
+        $seq->display_name($final_name);
+        $seq->description($hierarchy.($o_qiime ? "" : "."));
+        $out->write_seq($seq);      
+    } else {
+        ## Flag missing taxonomic ranks
+        ++$n_unidentified;
+        print UNIDENTIFIED "$ID\t$taxon_descriptors\n";
+    }
 }
-warn( "..done\n") if $debug;
-
-if($unidentified)
-{
-  print "Some organisms were not found in the taxonomy database.\n";
-  print "These organisms are in the \"$filename\.unidentified\" file.\n";
+print STDERR "Total sequences seen                    : $n_seqs\n";
+print STDERR "Total sequences identified and included : ".($id1 - 1)."\n";
+print STDERR "Organisms not found in taxonomy database: $n_unidentified".($n_unidentified ? "    See '$outfile_unidentified'." : "")."\n";
+if ($regexp_excluded) {
+    print STDERR "Organisms excluded by matching '$regexp_excluded': $n_excluded".($n_excluded ? "    See '$outfile_excluded'." : "")."\n";
+} else {
+    print STDERR "No --exclude option provided.\n";
 }
-close(UNID);
-close(OUT);
+
+close(UNIDENTIFIED);
+close(TAXONOMY);
